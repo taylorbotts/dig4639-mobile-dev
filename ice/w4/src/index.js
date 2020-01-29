940a9b93f2a006714270539dc4a33e78fdc93503
Taylor <taylorbotts@knights.ucf.edu>
2020-01-29 12:14:20 -0500

    Finish ICE

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -2,7 +2,7 @@ import Component from "./Component.js";
 import Task from "./Task.js";
 function runOnLoad()
 {
-    
+    console.log("new change");
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
@@ -11,6 +11,17 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task({
+        content: "Finish ICE exercise",
+        done: false
+    }
+    );
+    var container = document.getElementById("taskList");
+    var newtask = document.createElement("div");
+    newtask.innerHTML = myTask.render();
+    container.appendChild(newtask);
+    console.log(myTask.render());
+    
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
