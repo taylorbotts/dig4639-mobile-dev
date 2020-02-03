a7d2974a1d8262d5c52c6d871fe58d4999588802
Taylor <taylorbotts@knights.ucf.edu>
2020-02-03 12:11:09 -0500

    ice w5a

--- ice/w5a/src/index.js
+++ ice/w5a/src/index.js
@@ -1,16 +1,26 @@
+import Task from "./components/Task";
+
+let element;
+let inputElement;
+
 function runOnLoad()
 {
     // Create a container for us
-    let element = document.createElement("div");
+    element = document.createElement("div");
     element.id = "container";
     document.body.appendChild(element);
 
     // Handle adding a new task
     var addTaskButton = document.getElementById("addTask");
+    inputElement = document.getElementById("taskText");
     addTaskButton.addEventListener("click", onClick)
 }
 function onClick() {
-    console.log("clicked!");
+    var newTask = new Task({
+        content: inputElement.value
+    })
+    element.appendChild(newTask.render());
+
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
