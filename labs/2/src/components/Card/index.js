5071ecf8a1e9003e21b44e897b2e663b9024e639
Taylor <taylorbotts@knights.ucf.edu>
2020-02-07 18:39:38 -0500

    done with 1pt extra credit

--- labs/2/src/components/Card/index.js
+++ labs/2/src/components/Card/index.js
@@ -3,11 +3,23 @@ import './index.css';
 class Card {
     constructor(props){
         this.props = props;
-        
+        this.element = document.createElement("div");
+        this.element.className = "card";
+        this.element.innerHTML = this.props.content;
+        this.onClick = this.onClick.bind(this);
+        this.element.addEventListener("click",this.onClick);
+    }
+
+    onClick(){
+        if(this.element.innerHTML == this.props.question){
+            this.element.innerHTML = this.props.answer;
+        } else {
+            this.element.innerHTML = this.props.question;
+        }
     }
 
     render(){
-
+        return this.element;
     }
 }
 
--- labs/2/src/index.js
+++ labs/2/src/index.js
@@ -5,8 +5,13 @@ function runOnLoad(){
     let element = document.createElement("div");
     element.id = "container";
     document.body.appendChild(element);
-    var newCard = new Card({content:"Sample value provided"});
+    var newCard = new Card({
+        content:"Sample value provided",
+        question:"Is this right?",
+        answer:"I think so"
+    });
     element.appendChild(newCard.render());
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
+
