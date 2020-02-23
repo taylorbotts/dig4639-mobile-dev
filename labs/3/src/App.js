a24829005eb392beda5b0dd758715ae24cce50bc
Taylor <taylorbotts@knights.ucf.edu>
2020-02-23 11:31:01 -0500

    lab 3 complete

---
+++ labs/3/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card/'
+
+function App() {
+  return (
+    <Card content="This is a card!"></Card>
+  )
+}
+
+export default App;
